6c152c7d27b4909f3a025c378f4cd11df00e9e62
arshedarif <arifarshed2002@gmail.com>
2025-07-14 23:30:33 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def generate_seo_filename(title: str) -> str:
-    title = title.lower()
-    title = re.sub(r'[^a-z\s]', '', title)
-    title = re.sub(r'\s+', '-', title)
-    title = re.sub(r'-+', '-', title)
-    title = title.strip('-')
-    return title
